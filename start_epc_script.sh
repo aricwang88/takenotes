182c0ce7c1e7824c20245b6d2bd1af772422655c
Aric Wang <ehhewng07@gmail.com>
2017-06-23 14:18:18 +0800

    Update start_epc_script.sh

--- start_epc_script.sh
+++ start_epc_script.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 EXE_RUN_PATH=`pwd`
@@ -7,26 +6,26 @@ if [ $1 = "start" ]; then
     echo 'Setup core network.'
     if [ $2 = 'all' ]; then
         echo '===> Start all nodes in EPC, HSS/SPGW/MME.'
-        /usr/bin/xterm -fg Brown -bg Cyan -cr Yellow -fn 9*15 -e "pushd /home/ehhewng/openair-cn/SCRIPTS/; source ../oaienv; ./run_hss" &
-        sleep 1
-        /usr/bin/xterm -fg lightgreen -bg Blue -cr red -fn 9*15 -e "pushd /home/ehhewng/openair-cn/SCRIPTS/; source ../oaienv; ./run_spgw" &
-        sleep 1
-        /usr/bin/xterm -fg LightYellow -bg Orange -cr red -fn 9*15 -e "pushd /home/ehhewng/openair-cn/SCRIPTS/; source ../oaienv; ./run_mme" &
-        sleep 1
-        /usr/bin/xterm -fg lightgreen -bg LightYellow -cr red -fn 9*15 -e "tailf /tmp/mme.log" &
+        /usr/bin/xterm -fg Yellow -bg Black -cr DarkRed -e "pushd /home/ehhewng/openair-cn/SCRIPTS/; source ../oaienv; ./run_hss" &
+	sleep 1
+        /usr/bin/xterm -fg lightgreen -bg Blue -cr red  -e "pushd /home/ehhewng/openair-cn/SCRIPTS/; source ../oaienv; ./run_spgw" &
+	sleep 1
+        /usr/bin/xterm -fg LightYellow -bg Orange -cr red -e "pushd /home/ehhewng/openair-cn/SCRIPTS/; source ../oaienv; ./run_mme" &
+	sleep 1
+	/usr/bin/xterm -fg LightGreen -bg black -cr red -e "tailf /tmp/mme.log" &
 
     fi
-
+    
     if [ $2 = 'hss' ]; then
-        echo 'Start HSS nodes in EPC.'
-    fi
+	echo 'Start HSS nodes in EPC.'
+    fi 
 
     if [ $2 = 'spgw' ]; then
-        echo 'Start SPGW nodes in EPC.'
+	echo 'Start SPGW nodes in EPC.'
     fi
 
     if [ $2 = 'mme' ]; then
-        echo 'Start MME nodes in EPC.'
+	echo 'Start MME nodes in EPC.'
     fi
 
 fi
@@ -38,17 +37,17 @@ if [ $1 = "stop" ]; then
     if [ $2 = 'all' ]; then
         echo '===> Stop all nodes in EPC, HSS/SPGW/MME.'
     fi
-
+    
     if [ $2 = 'hss' ]; then
-        echo 'Stop HSS nodes in EPC.'
-    fi
+	echo 'Stop HSS nodes in EPC.'
+    fi 
 
     if [ $2 = 'spgw' ]; then
-        echo 'Stop SPGW nodes in EPC.'
+	echo 'Stop SPGW nodes in EPC.'
     fi
 
     if [ $2 = 'mme' ]; then
-        echo 'Stop MME nodes in EPC.'
+	echo 'Stop MME nodes in EPC.'
     fi
 
 fi
